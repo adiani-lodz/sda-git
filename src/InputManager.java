f26c56d81ab98d677423c8d2f9d6fc9e140ee657
Adrian <adiani@interia.pl>
2019-03-03 09:37:17 +0100

    Update3

--- src/InputManager.java
+++ src/InputManager.java
@@ -1,9 +1,20 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class InputManager {
+
+    private static Scanner scanner = new Scanner(System.in);
+
     public static String getOperation() {
-        throw new NoSuchMethodError();
+        System.out.println("Podaj znak operacji matematycznej," + " którą chcesz wykonać");
+        return scanner.next();
+
+//        return JOptionPane.showInputDialog("Podaj znak operacji matematycznej," + "którą chcesz wykonać");
+//        //dlatego plus bo sie nie miesci w jednej lini
     }
 
     public static String getNumber() {
-        throw new NoSuchMethodError();
+        System.out.println("Podaj liczbę dla której chcesz wykonac operację");
+        return scanner.next();
     }
 }
