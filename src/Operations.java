19b43f5f099ed96700673a720f0486718344b894
Adrian <adiani@interia.pl>
2019-03-03 09:43:44 +0100

    Update4

--- src/Operations.java
+++ src/Operations.java
@@ -1,5 +1,19 @@
 public class Operations {
-    public static int giveAnswer() {
-        throw new NoSuchMethodError();
+    public static int giveAnswer(String znakDzialania, String liczba1, String liczba2) {
+        switch (znakDzialania){
+            case "+":
+                return Integer.valueOf(liczba1) + Integer.valueOf( liczba2);
+                //zadeklarowalismy ze liczba to string wiec musimy zaienic ja na INT
+            case "-":
+                return Integer.valueOf(liczba1) - Integer.valueOf( liczba2);
+            case "*":
+                return Integer.valueOf(liczba1) * Integer.valueOf( liczba2);
+            case "/":
+                return Integer.valueOf(liczba1) / Integer.valueOf( liczba2);
+                default:
+                    throw new IllegalArgumentException();
+                    //jezlei umiescimy tutaj wyjatek to dowiemy sie od razu ze cos jest nie tak ze znakiem niz bedzie to krazylo po kodzie
+        }
+
     }
 }
