fb03b8a212ec09428a01ec3496e6eae590f2032a
Adrian <adiani@interia.pl>
2019-03-03 10:00:16 +0100

    Update6

--- src/KalkulatorApp.java
+++ src/KalkulatorApp.java
@@ -1,7 +1,12 @@
 public class KalkulatorApp {
     public static void main(String[] args) {
         String działanie = InputManager.getOperation();
-        OperationValidator.validate(działanie); //todo obsługa wyjatku
+        try {
+            OperationValidator.validate(działanie); //todo obsługa wyjatku
+        } catch (IllegalArgumentException e ) {
+            e.printStackTrace();
+            działanie = InputManager.getOperation();
+        }
 
         String liczba1 = InputManager.getNumber(); //todo obsługa wyjatku
         NumberValidator.validate(liczba1);
