2e59227698fa38632bd855bc8d2e885416579461
Adrian <adiani@interia.pl>
2019-03-03 09:14:07 +0100

    Update

--- src/OperationValidator.java
+++ src/OperationValidator.java
@@ -1,8 +1,11 @@
 public class OperationValidator {
     public static void validate(String działanie) {
-
+        //sprawdzamy czy dlugosc stronga jest 1 lub mniejsza
+        //przewidujemy tylko pojedynczy znak
         if (działanie.trim().length() > 1 &&
+                //sprawdzamy czy znak jest znakiem operacji
                 !działanie.trim().matches("/ [+-/*]/g"))
+            //jezeli warunek jest spelniony rzucamy wyjatek
             throw new UnsupportedOperationException(
                     "To nie jest znak działania matematycznego");
     }
