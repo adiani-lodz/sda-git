61938c4a2e0aa9781b10c522d7806a2d51f66893
Adrian <adiani@interia.pl>
2019-03-03 09:51:30 +0100

    Update5

--- src/NumberValidator.java
+++ src/NumberValidator.java
@@ -1,6 +1,6 @@
 public class NumberValidator {
     public static void validate(String liczba1) {
-        if (!liczba1.trim().matches("\\d"))
+        if (!liczba1.trim().matches("\\d+"))
         throw new IllegalArgumentException("To nie jest liczba");
     }
 }
